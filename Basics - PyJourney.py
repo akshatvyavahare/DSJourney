dbbbb2cc81ae69fedc94a82f82df7b53ab61dab6
Akshat V <32032916+akshatvyavahare@users.noreply.github.com>
2022-04-18 07:54:08 +0530

    Python Basics

---
+++ Basics - PyJourney.py
@@ -0,0 +1,85 @@
+# akshatv.com
+
+# About
+# My name is Akshat and I am here to start my journey to become a Data Scientist, a tableau Visualizer.
+# Lets begein our journey with Python Basics, then Pandas, beautiful soup, matplotlib.
+# We'll then move towards data extraction or webscraping from the web to collect the recent data and visualize the data in Tableau
+# you can visit my website for more interesting things akshatv.com
+
+
+# Starting with Python
+
+# print text
+print("hello and welcome to my Python Journey!")
+
+# print number
+print("12")
+
+# assign values to a variable
+x = 15
+y = 12
+print(x)                      #when printing variables it is without the inverted commas
+print(y)
+
+# types of objects            #Type             Indicator   - Explanation
+a = 1                         #integer          ('int')     - whole numbers
+b = 1.5                       #Float_Number     ('float')   - numbers with a decimal
+c = 'Hello World'             #String           ('str')     - any kind of text is a string
+d = True                      #Boolean          ('bool')    - a boolean is always without quotes
+
+#Lets print and see the type of each object
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#basic mathematics
+w = 2+2 
+print(w)
+
+x = 22-10
+print(x)
+
+y = 5*5
+print(y)
+
+z = 100/10
+print(z)
+
+a = 2**3
+print(a)
+
+#Basic String operations
+x = "Python"
+print(x[0], x[1], x[2], x[3], x[4], x[5])
+
+print(x[0:4])
+print(x[3:6])
+print(x[:])
+
+print(x[::2])
+
+print(x+x)
+print(x*5)
+
+# replace text
+
+x = 'Python 101'
+print(x.replace('101', '202'))
+
+# Data Structures - Tuple
+
+x = ('item 1', '2', '5', 'object 0', '0.8')
+print(x)
+print(type(x))
+
+y = ('0', '30', '50', '20', '10', '40')
+z = sorted(y)
+print(z)
+
+# Data Structures - List
+# Data Structures - Dictionary
+# Data Structures - Set
+
+# Flow Chart - Branch - if else
+# Flow Chart - Loop - Elseif
